2d6f9e31ffe8ca4a7e40fe1c30f82cb3c953fcea
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2024-09-25 10:26:30 +0300

    separate module and cli

--- src/pypackage/cli.py
+++ src/pypackage/cli.py
@@ -1,5 +1,15 @@
+import argparse
+from .module import greet
+
 def cli():
-    print("Hello world!")
+    # Parse arguments
+    parser = argparse.ArgumentParser(description="Command line interface")
+    parser.add_argument('name', type=str, help="Name")
+    args = parser.parse_args()
+
+    # Run library code
+    greet(args.name)
 
 if __name__ == "__main__":
+    # Entrypoint to the command line interface
     cli()

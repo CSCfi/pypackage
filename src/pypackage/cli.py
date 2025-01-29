a74fdd6119c10f09379f11818fc6486a9ae9fb9c
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2025-01-29 11:55:25 +0200

    changes

--- src/pypackage/cli.py
+++ src/pypackage/cli.py
@@ -1,16 +1,18 @@
 import argparse
 from .module import greet
 
+
 def main():
     """Entrypoint to the command line interface"""
 
     # Parse arguments
     parser = argparse.ArgumentParser(description="Command line interface")
-    parser.add_argument('name', type=str, help="Name")
+    parser.add_argument("name", type=str, help="Name")
     args = parser.parse_args()
 
     # Run library code
     greet(args.name)
+
 
 if __name__ == "__main__":
     # Facilitate scripting usage
